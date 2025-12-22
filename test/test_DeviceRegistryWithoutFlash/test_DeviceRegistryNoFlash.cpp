dd676b3094604b0e1d7004787e6e973e2f943800
y-decimal <83334510+y-decimal@users.noreply.github.com>
2025-12-22 23:44:52 +0100

    refactor: update device identification in tests from string-based to uint8_t ID

--- test/test_DeviceRegistryWithoutFlash/test_DeviceRegistryNoFlash.cpp
+++ test/test_DeviceRegistryWithoutFlash/test_DeviceRegistryNoFlash.cpp
@@ -24,10 +24,10 @@ void test_USE_FLASH_true(void)
 void test_getDeviceMac_found(void)
 {
     uint8_t testMac[6] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
-    const char *deviceName = "TestDevice";
-    registry.addDevice(deviceName, testMac);
+    uint8_t deviceID = 194;
+    registry.addDevice(deviceID, testMac);
 
-    const uint8_t *returnedMac = registry.getDeviceMac(deviceName);
+    const uint8_t *returnedMac = registry.getDeviceMac(deviceID);
     TEST_ASSERT_NOT_NULL(returnedMac);
     TEST_ASSERT_EQUAL_UINT8_ARRAY(testMac, returnedMac, 6);
 }
@@ -36,7 +36,7 @@ void test_getDeviceMac_not_found(void)
 {
     uint8_t testMac[6] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66};
 
-    const uint8_t *returnedMac = registry.getDeviceMac("NonExistentDevice");
+    const uint8_t *returnedMac = registry.getDeviceMac(86);
     TEST_ASSERT_NULL(returnedMac);
 }
 
@@ -44,13 +44,13 @@ void test_getUpdateDeviceMac_found(void)
 {
     uint8_t initialMac[6] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55};
     uint8_t newMac[6] = {0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB};
-    const char *deviceName = "UpdateDevice";
+    uint8_t deviceID = 31;
 
-    registry.addDevice(deviceName, initialMac);
-    bool updateResult = registry.updateDeviceMac(deviceName, newMac);
+    registry.addDevice(deviceID, initialMac);
+    bool updateResult = registry.updateDeviceMac(deviceID, newMac);
     TEST_ASSERT_TRUE(updateResult);
 
-    const uint8_t *returnedMac = registry.getDeviceMac(deviceName);
+    const uint8_t *returnedMac = registry.getDeviceMac(deviceID);
     TEST_ASSERT_NOT_NULL(returnedMac);
     TEST_ASSERT_EQUAL_UINT8_ARRAY(newMac, returnedMac, 6);
 }
@@ -58,9 +58,9 @@ void test_getUpdateDeviceMac_found(void)
 void test_getUpdateDeviceMac_not_found(void)
 {
     uint8_t newMac[6] = {0xDE, 0xAD, 0xBE, 0xEF, 0x00, 0x01};
-    const char *deviceName = "NonExistentDevice";
+    uint8_t deviceID = 64;
 
-    bool updateResult = registry.updateDeviceMac(deviceName, newMac);
+    bool updateResult = registry.updateDeviceMac(deviceID, newMac);
     TEST_ASSERT_FALSE(updateResult);
 }
 
@@ -68,17 +68,17 @@ void test_getDeviceMac_multiple_devices(void)
 {
     uint8_t mac1[6] = {0x10, 0x20, 0x30, 0x40, 0x50, 0x60};
     uint8_t mac2[6] = {0x70, 0x80, 0x90, 0xA0, 0xB0, 0xC0};
-    const char *deviceName1 = "DeviceOne";
-    const char *deviceName2 = "DeviceTwo";
+    uint8_t deviceID1 = 23;
+    uint8_t deviceID2 = 24;
 
-    registry.addDevice(deviceName1, mac1);
-    registry.addDevice(deviceName2, mac2);
+    registry.addDevice(deviceID1, mac1);
+    registry.addDevice(deviceID2, mac2);
 
-    const uint8_t *returnedMac1 = registry.getDeviceMac(deviceName1);
+    const uint8_t *returnedMac1 = registry.getDeviceMac(deviceID1);
     TEST_ASSERT_NOT_NULL(returnedMac1);
     TEST_ASSERT_EQUAL_UINT8_ARRAY(mac1, returnedMac1, 6);
 
-    const uint8_t *returnedMac2 = registry.getDeviceMac(deviceName2);
+    const uint8_t *returnedMac2 = registry.getDeviceMac(deviceID2);
     TEST_ASSERT_NOT_NULL(returnedMac2);
     TEST_ASSERT_EQUAL_UINT8_ARRAY(mac2, returnedMac2, 6);
 }
@@ -86,21 +86,21 @@ void test_getDeviceMac_multiple_devices(void)
 void test_removeDevice_found(void)
 {
     uint8_t testMac[6] = {0xAB, 0xCD, 0xEF, 0x12, 0x34, 0x56};
-    const char *deviceName = "RemovableDevice";
+    uint8_t deviceID = 75;
 
-    registry.addDevice(deviceName, testMac);
-    bool removeResult = registry.removeDevice(deviceName);
+    registry.addDevice(deviceID, testMac);
+    bool removeResult = registry.removeDevice(deviceID);
     TEST_ASSERT_TRUE(removeResult);
 
-    const uint8_t *returnedMac = registry.getDeviceMac(deviceName);
+    const uint8_t *returnedMac = registry.getDeviceMac(deviceID);
     TEST_ASSERT_NULL(returnedMac);
 }
 
 void test_removeDevice_not_found(void)
 {
-    const char *deviceName = "NonExistentDevice";
+    uint8_t deviceID = 82;
 
-    bool removeResult = registry.removeDevice(deviceName);
+    bool removeResult = registry.removeDevice(deviceID);
     TEST_ASSERT_FALSE(removeResult);
 }
 
